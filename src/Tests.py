182b17386d55fd704af65577530bade6b499e607
honeyysharma <honey.y.sharma@gmail.com>
2017-03-16 22:49:14 +0530

    Backdrop alignment works fine.

--- src/Tests.py
+++ src/Tests.py
@@ -1,55 +1,34 @@
-node = nuke.toNode("BackdropNode1")
-print node.height(), node.width()
-print node.screenHeight(), node.screenWidth()
-
-width = node.screenWidth()
-height = node.screenHeight()
-ypos = node.ypos()
-xpos = node.xpos()
-
-for i in range(1,4):
-    nodeNew = nuke.createNode("BackdropNode")
-    nodeNew.setXpos(xpos)
-    nodeNew.setYpos(ypos+height+50)
-    height = nodeNew.screenHeight()
-    xpos = nodeNew.xpos()
-    ypos = nodeNew.ypos()
-    
-"""
 import nuke
 
-templates = ["/homes/sharmah/nuke/temp1.nk", "/homes/sharmah/nuke/temp2.nk"]
+files = ["C:\Users\priyansh shama\Documents\Nuke\Template\envir.nk",
+        "C:\Users\priyansh shama\Documents\Nuke\Template\char.nk",
+        "C:\Users\priyansh shama\Documents\Nuke\Template\crowd.nk"]
 
-nuke.scriptReadFile(templates[0])
+def buildCompScript():
+    templates = files
 
+    for i, template in enumerate(templates):
+        nuke.scriptReadFile(template)
+        nuke.selectAll()
+        node = nuke.selectedNodes("BackdropNode")[0]
 
+        if i > 0:
+            bdNodes = node.getNodes()
+            nodePrevX = node.xpos()
+            nodePrevY = node.ypos()
+            node.setYpos(previousNode.ypos() + 500)
+            node.setXpos(previousNode.xpos())
 
-bd1 = nuke.toNode("BackdropNode1")
-print bd1.ypos()
+            for n in bdNodes:
+                if n.ypos() > nodePrevY:
+                    n.setYpos(node.ypos()+(n.ypos()-nodePrevY))
+                else:
+                    n.setYpos(node.ypos()+(n.ypos()+nodePrevY))
+                if n.xpos() > nodePrevX:
+                    n.setXpos(node.xpos()+(n.xpos()-nodePrevX))
+                else:
+                    n.setXpos(node.xpos()+(n.xpos()+nodePrevX))
+        
+        previousNode = node
 
-bd2 = nuke.toNode("BackdropNode2")
-print bd2.ypos()
-
-
-bdNodes = bd2.getNodes()
-bd2PrevX = bd2.xpos()
-bd2PrevY = bd2.ypos()
-
-bd2.setYpos(bd1.ypos() + 500)
-bd2.setXpos(bd1.xpos())
-
-
-print "Bd nodes"
-for n in bdNodes:
-    print n.name(),n.ypos()
-    if n.ypos() > bd2PrevY:
-        n.setYpos(bd2.ypos()+(n.ypos()-bd2PrevY))
-    else:
-        n.setYpos(bd2.ypos()+(n.ypos()+bd2PrevY))
-
-    if n.xpos() > bd2PrevX:
-        n.setXpos(bd2.xpos()+(n.xpos()-bd2PrevX))
-    else:
-        n.setXpos(bd2.xpos()+(n.xpos()+bd2PrevX))
-
-"""
+buildCompScript()
